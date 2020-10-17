5371db448dbcb237848147c133db191771c06001
noctu13 <13bit57@gmail.com>
2020-10-17 16:22:03 +0300

    v1.0e

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/"),]
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
